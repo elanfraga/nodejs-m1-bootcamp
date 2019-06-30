8ced6e94da2c26a3af1283badaf3798c4ec07b82
Elan Fraga <elanfraga@gmail.com>
2019-06-30 18:11:43 -0300

    CRUD

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const server = express();
+
+server.use(express.json());
+
 // Query parans
 server.get("/teste", (req, res) => {
   const nome = req.query.nome;
@@ -8,12 +11,45 @@ server.get("/teste", (req, res) => {
   return res.json({ message: `Hello ${nome}` });
 });
 
-// Route params
-server.get("/users/:id", (req, res) => {
-  // desestruturação
-  const { id } = req.params;
+const users = ["Diego", "Robson", "Victor"];
 
-  return res.json({ message: `Buscando o usuário ${id}` });
+// CRUD - Create, Read, Update, Delete
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+// Route params
+server.get("/users/:index", (req, res) => {
+  // desestruturação
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
